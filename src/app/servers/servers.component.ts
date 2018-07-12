212d58a3cad80e819cd92d34885d36b6f17f867d
jromano8954 <jromano6556@gmail.com>
2018-07-12 19:22:39 -0400

    Added example of Two-Way Data Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No Server was Created";
-  serverName = "";
+  serverName = "Test Server Here";
 
   constructor() { 
     setTimeout(() => {
